c2cf09cd0b5d2244bf8d92888aa70d2adcd3a018
idontknow83 <manoncristel37@gmail.com>
2024-09-17 14:55:02 +0200

    added nav-bar

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../resources/css/home.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     @yield ('title')
 </head>
 <body>
-
+<ul>
+  <li><a href="/home/">praktijk</a></li>
+  <li><a href="/news/">over ons</a></li>
+  <li><a href="/contact/">nieuws</a></li>
+  <li><a href="/about/">spoed</a></li>
+  <span class="material-symbols-outlined">person</span>
+  
+</ul>
 </body>
 </html>
