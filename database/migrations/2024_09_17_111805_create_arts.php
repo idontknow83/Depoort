31a508afb08e46873f3a9fac8ead3b5a7af3200c
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-09-17 14:18:36 +0200

    feat: added database migrations

---
+++ database/migrations/2024_09_17_111805_create_arts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('arts', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('voornaam');
+            $table->string('tussenvoegsel');
+            $table->string('achternaam');
+            $table->string('adres');
+            $table->string('postcode');
+            $table->string('woonplaats');
+            $table->string('land');
+            $table->string('telefoon');
+            $table->string('mobiel');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('arts');
+    }
+};
