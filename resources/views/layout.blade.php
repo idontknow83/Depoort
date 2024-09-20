754f42a179f18f4dcf92623617f2aa5082260607
idontknow83 <manoncristel37@gmail.com>
2024-09-20 09:32:09 +0200

    Added registerpage

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../resources/css/home.css">
+    <link rel="stylesheet" href="../resources/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    @yield ('title');
+    @yield ('title')
 </head>
 <body>
 <ul>
@@ -15,7 +15,7 @@
   <li><a href="/contact/">nieuws</a></li>
   <li><a href="/about/">spoed</a></li>
   <span class="material-symbols-outlined">person</span>
-  
 </ul>
+@yield ('content')
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,17 +1,51 @@
-@extends ('layout');
+@extends ('layout')
 
 @section ('title')
     <title>Register</title>
 @endsection
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@section ('content')
+    <div>
+        <form action="/action_page.php" class="register-form" method="POST">
+            <label for="fname">Naam:</label>
+            <div>
+                <input type="text" id="vnaam" name="vnaam" placeholder="voornaam" required>
+                <input type="text" id="tv" name="tv" size="10" placeholder="tv" required>
+                <input type="text" id="anaam" name="anaam" placeholder="achternaam" required>
+            </div>
+            
+            <label for="telnummer">Telefoon:</label>
+            <input type="tel" id="telnummer" name="telnummer" placeholder="telefoon" pattern="[0-9]{10}" required>
+            
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="email" required>
+            
+            <label for="adres">Adres:</label>
+            <input type="text" id="adres" name="adres" placeholder="adres" required>
+            
+            <label for="wp">Woonplaats:</label>
+            <input type="text" id="wp" name="wp" placeholder="woonplaats" required>
+        
+            <label for="postcode">Postcode:</label>
+            <input type="text" id="postcode" name="postcode" placeholder="postcode" pattern="^[1-9][0-9]{3}\s?[A-Za-z]{2}$" required>
+        
+            <label for="land">Land:</label>
+            <input type="text" id="land" name="land" placeholder="land" required>
+        
+            <label for="ww">Wachtwoord:</label>
+            <input type="password" id="ww" name="ww" placeholder="wachtwoord" required minlength="8">
+            
+            <label id="geslacht">Geslacht:</label>
+            <div class="gender-group">      
+                <input type="radio" id="man" name="geslacht" value="man" required>
+                <label for="man">Man</label>
+                <input type="radio" id="vrouw" name="geslacht" value="vrouw" required>
+                <label for="vrouw">Vrouw</label>
+            </div>
+            
+            <label for="geboortedatum">Geboortedatum:</label>
+            <input type="date" id="gb" name="geboortedatum" value="1950-01-01" min="1950-01-01" max="2024-12-31" required/>
+            
+            <input type="submit" value="Register">
+        </form>
+        
+@endsection
