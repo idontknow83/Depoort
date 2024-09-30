3c80f9ca71e78cf6c3e3c9bab34e5c319ac67233
Redemptionmx <zain.daniel190@gmail.com>
2024-09-30 13:20:11 +0200

    more changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,45 @@
 @extends('layout')
 
-@section ('title')
-<title>Home</title>
+@section('title')
+    <title>Home</title>
 @endsection
 
 
-@section('head')
-    <!-- Google Fonts Import -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
-@endsection
 
 @section('content')
-    <div style="text-align: center;">
-        <!-- Image -->
-        <img src="isala.png" alt="De Poort Logo">
+    <div class="centered-container">
+      <!-- Large Text with Custom Font -->
+        <h1 class="main-header">Huisartsen De Poort</h1> 
+       
+        <img class="location-image" src="https://www.optisport.nl/sites/default/files/images/map%20Zwolle.JPG" alt="Locatie van De Poort in Zwolle">
 
-        <!-- Large Text with Custom Font -->
-        <h1 style="font-family: 'Montserrat', sans-serif; font-size: 60px; font-weight: bold; color: #333; margin-top: 20px;">
-            Huisartsen De Poort
-        </h1>
+        
 
         <!-- Information about Isala Hospital Zwolle -->
-        <p style="font-family: 'Montserrat', sans-serif; font-size: 18px; color: #666; margin-top: 20px;">
-           De Poort in Zwolle is one of the largest hospitals in the Netherlands, offering a wide range of medical services and specialized care. With a focus on innovation and patient-centered care, Isala has become a leading healthcare provider in the region.
+       
+        <p class="info-paragraph">
+       
+       
+        De Poort in Zwolle is een van de grootste huisartsen in Nederland en biedt een breed scala aan medische diensten en specialistische zorg. Met een focus op innovatie en patiëntgerichte zorg is Isala uitgegroeid tot een toonaangevende zorgaanbieder in de regio.
         </p>
 
         <!-- Container for the two images with text -->
-        <div style="display: flex; justify-content: space-around; align-items: center; margin-top: 40px;">
+       
+       
+        <div class="flex-container">
             <!-- Left Image and Text -->
-            <div style="text-align: center; width: 40%;">
-                <img src="isala-building.jpg" alt="Isala Hospital Building" style="width: 100%; height: auto;">
-                <p style="font-family: 'Montserrat', sans-serif; font-size: 16px; color: #666; margin-top: 10px;">
-                    The modern architecture of De Poort in Zwolle, providing state-of-the-art facilities for patient care.
+            <div class="image-text-container">
+                <img src="https://th.bing.com/th/id/OIP.TYgR-i1tJvHGz-5ScrpOnwHaEJ?w=281&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Isala Hospital Building">
+                <p class="image-caption">
+                De moderne architectuur van De Poort in Zwolle biedt ultramoderne faciliteiten voor patiëntenzorg.
                 </p>
             </div>
 
             <!-- Right Image and Text -->
-            <div style="text-align: center; width: 40%;">
-                <img src="isala-staff.jpg" alt="Isala Hospital Staff" style="width: 100%; height: auto;">
-                <p style="font-family: 'Montserrat', sans-serif; font-size: 16px; color: #666; margin-top: 10px;">
-                    Dedicated healthcare professionals at De Poort, committed to delivering excellent patient care and medical services.
+            <div class="image-text-container">
+                <img src="https://th.bing.com/th/id/OIP.af-nqdoIoknzxz2QbpCOjAHaE8?w=303&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Isala Hospital Staff">
+                <p class="image-caption">
+                Toegewijde zorgprofessionals bij De Poort streven ernaar uitstekende patiëntenzorg en medische diensten te leveren.
                 </p>
             </div>
         </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../resources/css/style.css">
-    <link rel="stylesheet" href="../resources/css/style2.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    @yield ('title')
-</head>
-<body>
-<ul>
-  <li><a href="/home/">praktijk</a></li>
-  <li><a href="/news/">over ons</a></li>
-  <li><a href="/contact/">nieuws</a></li>
-  <li><a href="/about/">spoed</a></li>
-  <span class="material-symbols-outlined">person</span>
-</ul>
-@yield ('content')
-</body>
-</html>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="../resources/css/style.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+       @yield ('title')
+  </head>
+  <body>
+  <ul>
+    <li><a href="/home/">praktijk</a></li>
+    <li><a href="/news/">over ons</a></li>
+    <li><a href="/contact/">nieuws</a></li>
+    <li><a href="/about/">spoed</a></li>
+    <span class="material-symbols-outlined">person</span>
+  </ul>
+  @yield ('content')
+  </body>
+  </html>
