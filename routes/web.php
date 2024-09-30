548d827a67d123cf04bc05615880540244a4b91b
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-09-30 11:42:44 +0200

    chore: removed comments from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\ArtsController;
-use App\Http\Controllers\ConsultController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-// Route::get('/', [ArtsController::class, 'showArts']);
-// Route::get('/', [ClientController::class, 'showClient']);
-// Route::get('/', [ConsultController::class, 'showConsult']);
-
 Route::get('/register', function () {
     return view('register');
 });
+
 Route::get('/login', function () {
     return view('login');
 });
