86f53214d396f65bcf0bd6624d5ce314ebb6d8e1
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-01 12:51:27 +0200

    fix: fixed double layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,25 +19,3 @@
   @yield ('content')
   </body>
   </html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../resources/css/style.css">
-    <link rel="stylesheet" href="../resources/css/style2.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    @yield ('title')
-</head>
-<body>
-<ul>
-  <li><a href="/home/">praktijk</a></li>
-  <li><a href="/news/">over ons</a></li>
-  <li><a href="/contact/">nieuws</a></li>
-  <li><a href="/about/">spoed</a></li>
-  <a href="http://localhost/depoort/public/login"<span class="material-symbols-outlined">person</span></a>
-</ul>
-@yield ('content')
-</body>
-</html>
