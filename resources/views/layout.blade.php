d5cc56a2126dc143b8743a2e4b989bd477c69af6
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-02 16:29:48 +0200

    feat: used config file to include css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="../resources/css/style.css">
+      <link rel="stylesheet" href="{{ config("app.url") }}/resources/css/style.css">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
        @yield ('title')
   </head>
