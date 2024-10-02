ef74c09647952d330b1f212d93015ac1b1460afe
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-02 14:38:05 +0200

    feat: installed breeze

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,17 @@
-@extends ('layout')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('title')
-    <title>Register</title>
-@endsection
-
-@section('content')
-    <div class="container">
-        <h1>Welkom ******</h1>
-        <div class="buttons">
-            <div class="gegevens">
-                <div class="icon user"></div>
-                <span class="material-symbols-outlined person1">person</span>
-                <p id="dashboard-gegevens-label">Gegevens</p>
-            </div>
-            <div class="consulten">
-                <div class="icon doc"></div>
-                <p>Consulenten</p>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
             </div>
         </div>
     </div>
-   
-    </body>
-@endsection
+</x-app-layout>
