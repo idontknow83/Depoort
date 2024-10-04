fc0faa6a47f6fe60457201f71081d99bef8c2d20
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-04 11:03:50 +0200

    chore: seeder creates users again

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('users')->insert([
+            'naam' => 'admin',
+            'telnummer' => '0123456789',
+            'email' => 'admin@example.com',
+            'adres' => 'None',
+            'land' => 'Admin',
+            'postcode' => '3306DB',
+            'woonplaats' => 'Admin',
+            'gender' => 'Other',
+            'geboortedatum' => '26-12-1791',
+            'password' => Hash::make( 'password'),
+        ]);
+
         // DB::table('client')->insert([
         //     'voornaam' => fake()->firstName(),
         //     'tussenvoegsel' => null,
