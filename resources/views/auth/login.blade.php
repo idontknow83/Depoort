ae7361e653fcbcfcd371e5ac8aea665533242f27
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-14 10:49:14 +0200

    feat: changed email to phone number in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,20 +7,15 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-label for="telnummer" :value="__('Telnummer')" />
+            <x-text-input id="telnummer" class="block mt-1 w-full" type="text" name="telnummer" :value="old('telnummer')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
