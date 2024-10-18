348ba40211c5b80162b245679cd624423fee7b58
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-18 08:55:04 +0200

    feat: added delete and show function in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function show() 
     {
-        $users = User::all();
-        echo $users;
+        $users = User::get();
+        return $users;
+    }
+
+    public function delete(string $id)
+    {
+        DB::table('users')->where('id', '=', $id)->delete();
+        return redirect(config('app_url') . '/users');
     }
 }
