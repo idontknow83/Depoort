7f6b4b5ed8fcf05236261d7ea06fdd430cdcfea3
Team64OO <150142792+Team64OO@users.noreply.github.com>
2024-10-21 10:23:35 +0200

    fix: changed consult path

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
         </div>
         @endpermission
         <div class="py-12">
-            <a href="{{ env('app_url') }}/public/consult" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ env('app_url') }}/public/agenda" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center items-center bg-white overflow-hidden shadow-sm sm:rounded-lg size-60 px-12">
                     <div class="flex items-center flex-col text-gray-900">
                         <svg class="w-16" xmlns="http://www.w3.org/2000/svg"
