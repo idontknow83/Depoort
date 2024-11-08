d0ebf127888541c59a894a4dfca419706e2c3f42
idontknow83 <manoncristel37@gmail.com>
2024-11-08 10:37:32 +0100

    feat: moved css to proper file

--- resources/views/components/success-msg.blade.php
+++ resources/views/components/success-msg.blade.php
@@ -1,5 +1,6 @@
-<div id="error-popup" style="display: flex; justify-content: center;">
-    <div style="margin: 5px; padding: 5px; border: 2px solid black; border-radius: 10px; font-weight: bold; color: white; background-color: {{ $color }}; display: flex; justify-content: center; align-items: center;">
+<link rel="stylesheet" href="{{ config("app.url") }}/resources/css/Component.css">
+<div id="error-popup">
+    <div id="message" style=" background-color: {{ $color }};">
         {{ $message }}
         <p onclick="dismissMessage()" style="cursor: pointer; padding-left: 5px; padding-right: 5px; margin: 0; color: black;">X</p>
     </div>
